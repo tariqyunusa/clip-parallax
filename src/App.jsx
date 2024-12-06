6dd6ef031e4524d598ee6515e714183613db5acc
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-12-06 20:58:13 +0100

    wrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { showcase } from './images'
 import Gallery from './assets/Gallery'
 
+const lerp = (start, end, factor) => start + (end - start) * factor
 
 function App() {
+  const [position, setPosition] = useState({x:0, y:0})
+  const targetPosition = useRef({x: 0, y: 0})
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      targetPosition.current = { x: e.clientX, y: e.clientY };
+      setPosition((prev) => ({
+        x: lerp(prev.x, targetPosition.current.x, 0.1),
+        y: lerp(prev.y, targetPosition.current.y, 0.1),
+      }));
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+ 
   return (
     <>
-    <section className="wrapper">
+    <section className="wrapper" >
       {showcase.map(({name, identifier}, idx) => (
-        <Gallery name={name} id={identifier}  key={idx}/>
+        <Gallery name={name} id={identifier}  key={idx} position={position}/>
       ))}
       {/* <Card /> */}
     </section>
--- src/assets/Gallery.jsx
+++ src/assets/Gallery.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Gallery = ({name, id}) => {
+const Gallery = ({name, id, position}) => {
   return (
     <section className='gallery'>
       <div className="img_container">
         <img src={`/${name}.jpg`} alt="" />
       </div>
-      <div className="card">
+      <div className="card" style={{left: position.x, top: position.y}}>
         <img src={`/${name}1.jpg`} alt="" />
       </div>
     </section>
